eee2ff8177ab6f162d961ffe5b80b0db0ccc9361
Marta Altimir <martaag18@gmail.com>
2024-12-26 18:56:44 +0100

    Implementing dynamic list of phrases with ngFor and parent-child communication using @Input

--- onBoarding/src/app/home/home.component.ts
+++ onBoarding/src/app/home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { EscenaComponent } from './escena/escena.component';
+import { StepsService } from '../services/steps.service';
+import { IStep } from '../models/i-step';
+
 
 @Component({
   selector: 'app-home',
   imports: [EscenaComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  steps: IStep[] = [];
+
+  constructor(private stepsService: StepsService){}
+
+  ngOnInit(): void {
+    this.steps = this.stepsService.getSteps();
+  }
 
 }
