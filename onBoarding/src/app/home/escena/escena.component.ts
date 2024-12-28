014a74d8f351d88b61beb6acf4b144d32a8e9c1a
Marta Altimir <martaag18@gmail.com>
2024-12-28 18:43:30 +0100

    Implementing animation and changing scss style

--- onBoarding/src/app/home/escena/escena.component.ts
+++ onBoarding/src/app/home/escena/escena.component.ts
@@ -1,14 +1,28 @@
-import { Component, Input } from '@angular/core'; //Input permite a un componente hijo recibir datos desde su componente padre
-import { IStep } from '../../models/i-step'; //Importar interfaz
+import { Component, Input } from '@angular/core';
+import { IStep } from '../../models/i-step';
 import { CommonModule } from '@angular/common';
-
+import { trigger, transition, style, animate, keyframes} from '@angular/animations';
 
 @Component({
   selector: 'app-escena',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './escena.component.html',
-  styleUrl: './escena.component.scss'
+  styleUrls: ['./escena.component.scss'],
+  animations: [
+    trigger('elegantSlide', [
+      // Avanzar 
+      transition(':increment', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('800ms ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
+      ]),
+      // Retroceder 
+      transition(':decrement', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate('800ms ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class EscenaComponent {
   @Input() receivedSteps: IStep[] = [];
@@ -16,19 +30,19 @@ export class EscenaComponent {
 
   previousStep() {
     if (this.currentStep > 0) {
-      this.currentStep--;
+      this.currentStep--; 
     }
   }
 
   nextStep() {
     if (this.currentStep < this.receivedSteps.length - 1) {
-      this.currentStep++;
+      this.currentStep++; 
     }
   }
 
-  showStep(index:number){
-    this.currentStep = index;
+  showStep(index: number) {
+    this.currentStep = index; 
   }
-
- 
 }
+
+
